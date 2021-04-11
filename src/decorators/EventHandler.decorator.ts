7e6c47090a38bafd1fda31d345c4fc40f3e78598
alejandrodk <ottoabarriosp@hotmail.com>
2021-04-10 21:10:42 -0300

    Update decorator name

--- src/decorators/EventHandler.decorator.ts
+++ src/decorators/EventHandler.decorator.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { MetadataKeys } from '../enums'; 
 
-export const EventHandlerDecorator = (...events: any[]): ClassDecorator => {
+export const HandleEvent = (...events: any[]): ClassDecorator => {
   return(target: any) => {
     Reflect.defineMetadata(MetadataKeys.LINK_EVENT, events, target);
     return target;
